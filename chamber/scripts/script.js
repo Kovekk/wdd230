8ac341d0a5e2d75ec505734847b6240782f8daa7
Kovekk <fishm3001@gmail.com>
2023-05-10 22:30:39 -0600

    making header and nav responsive to size

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -7,3 +7,9 @@ hamButton.addEventListener("click", toggleNav);
 function toggleNav() {
     nav.classList.toggle("active");
 }
+
+// add current date to header
+
+const currentDateTag = document.querySelector("#currentDate");
+let currentDate = new Date().toLocaleString("en-GB", {dateStyle: "full"});
+currentDateTag.innerHTML = currentDate;
