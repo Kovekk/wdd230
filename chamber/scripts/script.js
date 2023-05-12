6f169b057faed2313747d378c3d9221b4d281991
Kovekk <fishm3001@gmail.com>
2023-05-12 12:47:07 -0600

    adding footer and making site responsive

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -13,3 +13,28 @@ function toggleNav() {
 const currentDateTag = document.querySelector("#currentDate");
 let currentDate = new Date().toLocaleString("en-GB", {dateStyle: "full"});
 currentDateTag.innerHTML = currentDate;
+
+
+// add last modified to footer
+
+const lastModifiedTag = document.querySelector("#lastModified");
+
+const options = {
+    month: "numeric",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+};
+let lastUpdated = new Date(document.lastModified).toLocaleString("en-US", options);
+
+lastModifiedTag.innerHTML = `Last Modified: ${lastUpdated}`;
+
+// add copyright year to footer
+
+const copyright = document.querySelector("#copyright");
+
+const currentYear = new Date().toLocaleString("en-US", {year: "numeric"});
+
+copyright.innerHTML = `&copy;${currentYear} Provo Chamber`
