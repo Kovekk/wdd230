197a8e16945267dbb74c68a579628d6e27667598
Kovekk <rbrtlwry@gmail.com>
2023-05-17 20:26:02 -0600

    adding home page content

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -38,3 +38,17 @@ const copyright = document.querySelector("#copyright");
 const currentYear = new Date().toLocaleString("en-US", {year: "numeric"});
 
 copyright.innerHTML = `&copy;${currentYear} Provo Chamber`
+
+// show banner on monday and tuesday only
+
+const banner = document.querySelector("#banner");
+console.log(new Date().getDay());
+
+switch (new Date().getDay()) {
+    case 1:
+    case 2:
+        banner.classList.toggle("show-banner");
+        break;
+    default:
+        break;
+}
