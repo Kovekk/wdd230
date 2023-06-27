638f1d8161b23cf2e11cb0fc278276e6e9e342d6
Kovekk <fishm3001@gmail.com>
2023-06-27 16:15:33 -0600

    adding validation for no entry

--- lesson4_bom/scripts/script.js
+++ lesson4_bom/scripts/script.js
@@ -1,21 +1,26 @@
 function addChapter() {
     const chapter = document.querySelector("#favchap");
-    const favList = document.querySelector("#list");
+    if (chapter.value == "") {
+        document.querySelector("main p").classList.add("noEntry");
+    } else {
+        document.querySelector("main p").classList.remove("noEntry");
+        const favList = document.querySelector("#list");
 
-    const newLi = document.createElement("li");
-    newLi.textContent = chapter.value;
-    chapter.value = "";
+        const newLi = document.createElement("li");
+        newLi.textContent = chapter.value;
+        chapter.value = "";
 
-    const deleteBtn = document.createElement("input");
-    deleteBtn.setAttribute("type", "button");
-    deleteBtn.setAttribute("value", "X");
-    deleteBtn.setAttribute("class", "delete-button");
-    deleteBtn.addEventListener("click", delChapter);
+        const deleteBtn = document.createElement("input");
+        deleteBtn.setAttribute("type", "button");
+        deleteBtn.setAttribute("value", "X");
+        deleteBtn.setAttribute("class", "delete-button");
+        deleteBtn.addEventListener("click", delChapter);
 
-    newLi.appendChild(deleteBtn);
-    favList.appendChild(newLi);
+        newLi.appendChild(deleteBtn);
+        favList.appendChild(newLi);
 
-    chapter.focus()
+        chapter.focus()
+    }
 }
 
 function delChapter(self) {
