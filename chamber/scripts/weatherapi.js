2508b188614a1c7f1c4de99b2618575994cd60fb
Kovekk <fishm3001@gmail.com>
2023-06-27 15:05:51 -0600

    finishing current weather api call

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -22,6 +22,22 @@ function displayResults(weatherData) {
     const weatherDesc = document.querySelector("#weatherDesc");
     const windSpeed = document.querySelector("#windSpeed");
     const windChill = document.querySelector("#windChill");
+
+    const currentTemp = weatherData.main.temp.toFixed(0);
+    const windSpeedNum = weatherData.wind.speed;
+
+    weatherSymbol.setAttribute("src", `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`);
+    weatherSymbol.setAttribute("alt", weatherData.weather[0].description);
+    weatherDesc.textContent = weatherData.weather[0].description;
+    temp.textContent = `${currentTemp} F`;
+    windSpeed.textContent = `Wind Speed: ${windSpeedNum} mph`;
+    
+    if (currentTemp <= 50 && windSpeedNum > 3.0) {
+      const windChillNum = 35.74 + 0.6215*currentTemp - 35.75*(windSpeedNum**0.16) + 0.4275*currentTemp*(windSpeedNum**0.16);
+      windChill.textContent = `Feels Like:  ${windChillNum.toFixed(1)} F`;
+    } else {
+      windChill.textContent = "N/A";
+    }
 }
 
   apiFetch();
