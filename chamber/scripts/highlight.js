4127105c6a25346ae1d09da621ec5325a062bd41
Kovekk <fishm3001@gmail.com>
2023-06-29 09:39:03 -0600

    making highlights dynamic

--- chamber/scripts/highlight.js
+++ chamber/scripts/highlight.js
@@ -1,7 +1,7 @@
-const url = "data.json";
+const newurl = "data.json";
 
 async function getData() {
-    const response = await fetch(url);
+    const response = await fetch(newurl);
     const data = await response.json();
     displayData(data.directory);
 }
@@ -11,7 +11,51 @@ function filterByMembership (business) {
     return (membershipLevel == "Silver" || membershipLevel == "Gold");
 }
 
+function displayHighlight (member) {
+    // Create html elements
+    const section = document.createElement("section");
+    const nameH3 = document.createElement("h3");
+    const picture = document.createElement("picture");
+    const mediumSource = document.createElement("source");
+    const largeSource = document.createElement("source");
+    const smallImg = document.createElement("img");
+    const numberP = document.createElement("p");
+    const websiteA = document.createElement("a");
+
+    // putting picture element together
+    mediumSource.setAttribute("srcset", member.srcset[1]);
+    mediumSource.setAttribute("media", "(min-width:600px) and (max-width: 899px)");
+    largeSource.setAttribute("srcset", member.srcset[2]);
+    largeSource.setAttribute("media", "(min-width: 900px)");
+    smallImg.setAttribute("src", member.srcset[0]);
+    smallImg.setAttribute("alt", `${member.name} logo`);
+    picture.appendChild(mediumSource);
+    picture.appendChild(largeSource);
+    picture.appendChild(smallImg);
+
+    // putting phone number and website together
+    numberP.innerHTML = `${member.number} | `;
+    websiteA.innerHTML = "Website";
+    websiteA.setAttribute("href", member.website);
+    numberP.appendChild(websiteA);
+
+    // adding text to name
+    nameH3.innerHTML = member.name;
+
+    // putting everything into the section element
+    section.appendChild(nameH3);
+    section.appendChild(picture);
+    section.appendChild(numberP);
+
+    // add section to div#spotlight
+    document.querySelector("#spotlight").appendChild(section);
+}
+
 function displayData(business) {
     const highMembership = business.filter(filterByMembership);
-    
+    const highlightOne = displayHighlight(highMembership[4]);
+    const highlightTwo = displayHighlight(highMembership[1]);
+    const highlightThree = displayHighlight(highMembership[0]);
 }
+
+getData();
